eb03acd31131d1a3acba95432585e8c1ddb87c44
Elena Kulakova <egeorgievska89@gmail.com>
2019-12-02 13:45:42 +0100

    Implementing tableViewDelegate and dataSource

---
+++ ToDoListWithPersisting/Controller/ToDoViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  ToDoListWithPersisting
+//
+//  Created by Elena Kulakova on 2019-12-02.
+//  Copyright © 2019 Elena Kulakova. All rights reserved.
+//
+
+import UIKit
+
+class ToDoViewController: UITableViewController {
+
+    let itemArray = ["Make lunch", "Have a walk", "Learn Core Data"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    //MARK: UITableViewDataSours Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+    //MARK: UITableViewDelegate
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedItem = itemArray[indexPath.row]
+        print(selectedItem)
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+}
