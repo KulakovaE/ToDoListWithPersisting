dd74a2da09a576f3f09bef4be545ee8f717bfe50
Elena Kulakova <egeorgievska89@gmail.com>
2019-12-03 13:05:57 +0100

    Creating a custom Data Model and restriction the app with MVC. Refactoring with Ternary Operator. Adding newItem is crashing.

--- ToDoListWithPersisting/Controller/ToDoViewController.swift
+++ ToDoListWithPersisting/Controller/ToDoViewController.swift
@@ -10,40 +10,43 @@ import UIKit
 
 class ToDoViewController: UITableViewController {
 
-    var itemArray = ["Make lunch", "Have a walk", "Learn Core Data"]
+    var itemArray = [Item]()
     let defaults = UserDefaults.standard
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let items = defaults.array(forKey: "ToDoListArray") as? [String] {
+        let newItem = Item()
+        newItem.title = "Make Lunch"
+        itemArray.insert(newItem, at: 0)
+        
+        if let items = defaults.array(forKey: "ToDoListArray") as? [Item] {
             itemArray = items
         }
-        // Do any additional setup after loading the view.
     }
+    
     //MARK: UITableViewDataSours Methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = itemArray[indexPath.row].title
+        
+        cell.accessoryType = item.done ? .checkmark : .none
+        
         return cell
     }
     
     //MARK: UITableViewDelegate
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let selectedItem = itemArray[indexPath.row]
-        print(selectedItem)
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
-        
+        tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
@@ -57,18 +60,22 @@ class ToDoViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //what will happen once the user clicks on UIAlert
-            self.itemArray.insert(textField.text ?? "", at: 0)
-            //.append if the order is not important for you
+            let newItem = Item()
+            newItem.title = textField.text!
+            
+            self.itemArray.insert(newItem, at: 0)
             
             self.defaults.set(self.itemArray, forKey: "ToDoListArray")
+        
             self.tableView.reloadData()
         }
+        
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Create new item"
             textField = alertTextField
         }
+        
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
     }
-    
 }
---
+++ ToDoListWithPersisting/Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  ToDoListWithPersisting
+//
+//  Created by Elena Kulakova on 2019-12-03.
+//  Copyright © 2019 Elena Kulakova. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
