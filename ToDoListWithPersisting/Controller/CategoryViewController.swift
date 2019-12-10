55eeaf8f2e59f78319d744d440162143cb454f7c
Elena Kulakova <egeorgievska89@gmail.com>
2019-12-10 11:20:39 +0100

    Making Cell Swipeable with CocoaPods. Implementing inheritance from SwipeTableViewController.

--- ToDoListWithPersisting/Controller/CategoryViewController.swift
+++ ToDoListWithPersisting/Controller/CategoryViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import RealmSwift
 
-class CategoryViewController: UITableViewController {
+class CategoryViewController: SwipeTableViewController {
     
     let realm = try! Realm()
     
@@ -18,6 +18,7 @@ class CategoryViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         loadCategories()
+        tableView.rowHeight = 80.0
     }
     
     //MARK: - TableView DataSource Methods
@@ -27,7 +28,8 @@ class CategoryViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        
+        let cell = super.tableView(tableView, cellForRowAt: indexPath)
         
         cell.textLabel?.text = categories?[indexPath.row].name ?? "No Category added"
         
@@ -35,6 +37,7 @@ class CategoryViewController: UITableViewController {
     }
     
     //MARK: - Data Manipulation Methods
+    //MARK: - Save Data
     
     func save(category: Category) {
         
@@ -47,12 +50,27 @@ class CategoryViewController: UITableViewController {
         }
         self.tableView.reloadData()
     }
+    //MARK: - Load Data
     
     func loadCategories(){
         
         categories = realm.objects(Category.self)
         
         tableView.reloadData()
+    }
+    //MARK: - Delete Data From Swipe
+    
+    override func updateModel(at indexPath: IndexPath) {
+       
+        if let categoryForDeletion = self.categories?[indexPath.row] {
+            do {
+                try self.realm.write {
+                    self.realm.delete(categoryForDeletion)
+                }
+            } catch {
+                print("Error saving done status,\(error)")
+            }
+        }
     }
     
     
---
+++ ToDoListWithPersisting/Controller/SwipeTableViewController.swift
@@ -0,0 +1,54 @@
+//
+//  SwipeTableViewController.swift
+//  ToDoListWithPersisting
+//
+//  Created by Elena Kulakova on 2019-12-10.
+//  Copyright © 2019 Elena Kulakova. All rights reserved.
+//
+
+import UIKit
+import SwipeCellKit
+
+class SwipeTableViewController: UITableViewController, SwipeTableViewCellDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    //MARK: -TableView DataSouce Methods
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! SwipeTableViewCell
+        
+        cell.delegate = self
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> [SwipeAction]? {
+        
+        guard orientation == .right else { return nil }
+        
+        let deleteAction = SwipeAction(style: .destructive, title: "Delete") { action, indexPath in
+            //handle action by updating model with deletion
+            
+            self.updateModel(at: indexPath)
+            
+        }
+        // customize the action appearance
+        deleteAction.image = UIImage(named: "Delete")
+        
+        return [deleteAction]
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsOptionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> SwipeOptions {
+        var options = SwipeOptions()
+        options.expansionStyle = .destructive
+        return options
+    }
+    
+    func updateModel(at indexPath: IndexPath) {
+        //Update our dataModel
+    }
+}
